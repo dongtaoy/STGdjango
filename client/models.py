cdd9bf155a3a9f52485729b67cba25755b0ae1ca
georgecai904 <gc372222871@gmail.com>
2014-12-26 13:08:23 +1100

    client/models

--- client/models.py
+++ client/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+
+class Visa(models.Model):
+    name = models.CharField(max_length='50', null=True, blank=True)
+    subClass = models.IntegerField(max_length=3, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.subClass + " " + self.name
