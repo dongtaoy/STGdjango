fbb79a5fec5508e2d9bf020decbe7951e348b46a
georgecai904 <gc372222871@gmail.com>
2014-12-29 17:40:14 +1100

    dd

--- client/models.py
+++ client/models.py
@@ -1,8 +1,33 @@
 from django.db import models
+from hr.models import Employee
 
 
-# class Client(models.Model):
-# pass
+class Client(models.Model):
+    # Personal Information
+    name = models.CharField(max_length=100)
+    preferredName = models.CharField(max_length=100, blank=True, null=True)
+    DoB = models.DateField()
+    nationality = models.CharField(max_length=100)
+    Email = models.EmailField(blank=True, null=True)
+    contactNumber = models.CharField(max_length=20, blank=True, null=True)
+    onshore = models.BooleanField(blank=True, null=True)
+
+    # Visa Information
+    visa = models.ForeignKey(Visa)
+    expiryDate = models.DateField(blank=True, null=True)
+
+    # Service Information
+    status = models.CharField(max_length=100)
+    referal = models.ForeignKey(Employee, blank=True, null=True)
+    Consultant = models.ForeignKey(Employee, blank=True, null=True)
+    clientManager = models.ForeignKey(Employee, blank=True, null=True)
+    stage = models.ManyToManyField(Stage)
+    serviceFee = models.FloatField()
+    thirdPartyFeeReceived = models.FloatField(blank=True, null=True)
+    thirdPartyFeePaid = models.FloatField(blank=True, null=True)
+
+    # Others
+    note = models.TextField(blank=True, null=True)
 
 
 class Visa(models.Model):
@@ -24,8 +49,8 @@ class Stage(models.Model):
 
         # class StageDocuments(models.Model):
         # stage = models.ForeignKey(Stage)
-        #     title = models.CharField(max_length=100, null=True, blank=False)
-        #     path = models.CharField(max_length=150, null=True, blank=False)
+        # title = models.CharField(max_length=100, null=True, blank=False)
+        # path = models.CharField(max_length=150, null=True, blank=False)
         #
-        #     def __unicode__(self):
-        #         return self.title
+        # def __unicode__(self):
+        # return self.title
