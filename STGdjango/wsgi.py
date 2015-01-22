ab54e02991a9f3def1afa21ec1e56f589685772f
georgecai904 <gc372222871@gmail.com>
2015-01-22 13:03:40 +1100

    payment

--- STGdjango/wsgi.py
+++ STGdjango/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "STGdjango.settings")
 from django.contrib.auth.handlers.modwsgi import check_password
-os.environ["PYTHON_EGG_CACHE"] = '/tmp/'
+# os.environ["PYTHON_EGG_CACHE"] = '/tmp/'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
