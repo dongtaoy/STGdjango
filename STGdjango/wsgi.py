54d7c62ad232b9aa4e0dd24f632e2eed55cdc4db
georgecai904 <gc372222871@gmail.com>
2015-01-22 12:21:55 +1100

    payment

--- STGdjango/wsgi.py
+++ STGdjango/wsgi.py
@@ -7,9 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os,sys
-sys.path.append('/home/stg/STGdjango')
-os.environ["DJANGO_SETTINGS_MODULE"] = "STGdjango.settings"
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "STGdjango.settings")
 from django.contrib.auth.handlers.modwsgi import check_password
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
